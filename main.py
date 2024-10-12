026b91beb9fab26bea81b945001b7f71cf9ca4a5
Pedro Gonzalez Soro <pedrogonzalezsoro@MacBook-Air-de-Pedro.local>
2024-10-12 10:24:50 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from meta_ai_api import MetaAI
+
+ai = MetaAI() 
+
+app = Flask(__name__)
+CORS(app)  # Habilita CORS para todas las rutas
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/get_response/<user_text>', methods=['GET'])
+def get_response(user_text):
+    response = ai.prompt(message=user_text)
+    answer = response['message']
+    if response['sources']:
+        answer += f"\n\nFuentes: {', '.join(response['sources'])}"
+    print(answer)
+    return jsonify(answer)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
